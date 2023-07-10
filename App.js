1b6f57faaf3afbb85edaaa39ba7d65bb0f6cfd81
suruchi8269 <117128988+suruchi8269@users.noreply.github.com>
2023-07-10 10:54:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+// // import logo from './logo.svg';
+import './App.css';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import CandidateCard from './components/CandidateCard';
+function App() {
+  return (
+   <div>hello</div>
+  );
+}
+
+export default App;
